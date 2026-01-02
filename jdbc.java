30b20d944552ddc83abf69a9e7af6f05f2ec5c9c
Satoshi Fukuda <satoshi.fukuda@hashicorp.com>
2026-01-02 20:15:43 +0900

    Update jdbc.java

--- jdbc.java
+++ jdbc.java
@@ -7,8 +7,8 @@ import java.sql.ResultSet;
 public class SqlConnect {
     public static void main(String[] args) throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        final String jdbcId = "root";
-        final String jdbcPass = "";
+        final String jdbcId = "dbadmin";
+        final String jdbcPass = "passw0rd";
         final String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/";
         Connection cn = DriverManager.getConnection(jdbcUrl, jdbcId, jdbcPass);
         PreparedStatement st = cn.prepareStatement("show databases");
